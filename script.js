df6534267f1872d0f0715a672c1032fc176f80c9
Fabio Ribeiro <fcmribeiro22@gmail.com>
2023-03-29 15:42:42 +0100

    Add popup appear and disappear functionality

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
+// Button to form Popup
+
+// eslint-disable-next-line no-unused-vars
+function openForm() {
+  document.querySelector('.form-container').style.display = 'flex';
+  document.querySelector('.open-button').style.display = 'none';
+}
+
+// eslint-disable-next-line no-unused-vars
+function closeForm() {
+  document.querySelector('.form-container').style.display = 'none';
+  document.querySelector('.open-button').style.display = 'inline';
+}
+// Add books to program
+
 const myLibrary = [];
 
 // constructor
@@ -18,10 +33,10 @@ function Book(title, author, pages, isRead) {
   };
 }
 
-function addBookToLibrary() {
-  const title = 
-  const author = 
-  const pages = 
+/* function addBookToLibrary() {
+  const title =
+  const author =
+  const pages =
   const isRead = c
 
   const book = new Book(title, author, pages, isRead);
@@ -30,4 +45,4 @@ function addBookToLibrary() {
 }
 
 addBookToLibrary(); // prompts the user for input and adds a new book to myLibrary
-console.log(myLibrary); // prints the current state of the library
+console.log(myLibrary); // prints the current state of the library/*} */
