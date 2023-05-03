5f0ed16759e4ab9ac7716ab26cecfc305ebc8c61
Fábio Ribeiro <fcmribeiro22@gmail.com>
2023-05-03 17:21:19 +0100

    Insert books with DOM

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const openButton = document.querySelector('.open-button');
 const closeButton = document.querySelector('.close-button');
 const submitButton = document.querySelector('.submit-button');
+const bookGrid = document.querySelector('.book-container');
 
 openButton.addEventListener('click', () => {
   document.querySelector('.form-container').style.display = 'flex';
@@ -12,13 +13,20 @@ closeButton.addEventListener('click', () => {
   openButton.style.display = 'inline';
 });
 
-const myLibrary = [];
+const myLibrary = [{
+  title: 'asd', author: 'sdf', pages: '3', isRead: 'Read',
+},
+{
+  title: 'asdd', author: 'sdfd', pages: '33', isRead: 'Read',
+}];
 
 function resetForm() {
   document.getElementById('title').value = '';
   document.getElementById('author').value = '';
   document.getElementById('pages').value = '';
   document.getElementById('read_status').checked = false;
+  document.querySelector('.form-container').style.display = 'none';
+  openButton.style.display = 'inline';
 }
 
 // constructor
@@ -27,6 +35,33 @@ function Book(title, author, pages, isRead) {
   this.author = author;
   this.pages = pages;
   this.isRead = isRead;
+}
+
+function bookFromArray() {
+  for (let i = 0; i < myLibrary.length; i++) {
+    const book = myLibrary[i];
+
+    const bookCard = document.createElement('div');
+    bookCard.classList.add('book-card');
+
+    const title = document.createElement('h2');
+    title.textContent = book.title;
+    bookCard.appendChild(title);
+
+    const author = document.createElement('p');
+    author.textContent = `Author: ${book.author}`;
+    bookCard.appendChild(author);
+
+    const pages = document.createElement('p');
+    pages.textContent = `Pages: ${book.pages}`;
+    bookCard.appendChild(pages);
+
+    const read = document.createElement('p');
+    read.textContent = `Read: ${book.isRead}`;
+    bookCard.appendChild(read);
+
+    bookGrid.appendChild(bookCard);
+  }
 }
 
 function addBookToLibrary(event) {
@@ -39,9 +74,8 @@ function addBookToLibrary(event) {
 
   myLibrary.push(book);
   resetForm();
-  document.querySelector('.form-container').style.display = 'none';
-  openButton.style.display = 'inline';
-  console.log(myLibrary);
+  bookGrid.innerHTML = '';
+  bookFromArray();
 }
 
 submitButton.addEventListener('click', addBookToLibrary);
